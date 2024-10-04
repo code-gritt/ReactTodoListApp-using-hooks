ac54d75ce1176d0470c136e8e7e9329988deaf4d
gokul <gokulchandan24@gmail.com>
2024-10-04 20:42:42 +0530

    app initiated, header component created

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="app-wrapper">
+          <div>
+            <Header />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
